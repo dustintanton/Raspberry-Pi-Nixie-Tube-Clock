0387226870c2370d133fa73167e7ebf9778a5156
DustinTanton <dustin.tanton@gmail.com>
2019-12-18 15:26:04 -0600

    Massive rewrite adding functions for setting up pins, setting arrays of pins to numbers for display, and unsetting pins.

--- nixieclock.py
+++ nixieclock.py
@@ -1,115 +1,138 @@
+import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-import time
 
+# Time Variables Instances
+localtime = time.localtime(time.time())
+hour = localtime.tm_hour
+min = localtime.tm_min
+sec = localtime.tm_sec
+
+# 2nd digit seconds bulb
+bulb6 = ['a', 'b', 'c', 'd']
 a = 5
 d = 6
 b = 13
 c = 19
 
-A = 5
-D = 6
-B = 13
-C = 19
-
 BCD = [0000,0001,0010,0011,0100,0101,0110,0111,1000,1001]
 
-pin = "" 
-level = ""
-
-#Setup my channels
-GPIO.setup(a, GPIO.OUT)
-GPIO.setup(d, GPIO.OUT)
-GPIO.setup(b, GPIO.OUT)
-GPIO.setup(c, GPIO.OUT)
-
-GPIO.output(a, GPIO.LOW)
-GPIO.output(b, GPIO.LOW)
-GPIO.output(c, GPIO.LOW)
-GPIO.output(d, GPIO.LOW)
-
-#find time and set first variables to time values
-localtime = time.localtime(time.time())
-hour = localtime.tm_hour
-min = localtime.tm_min
-sec = localtime.tv_sec
+# Setup my channels
+setOn(bulb6)
 
 
+# find time and set first variables to time values
+
+def findTime():
+	localtime = time.localtime(time.time())
+	hour = localtime.tm_hour
+	min = localtime.tm_min
+	sec = localtime.tm_sec
 
 
-
-
-try:
-	while True:
+def set0(bulb):
 		# set address of the tube cathode '0':
-                		            #   ___ 
-		GPIO.output(A,GPIO.LOW)     #  |   |
-		GPIO.output(B,GPIO.LOW)     #  |   |
-		GPIO.output(C,GPIO.LOW)     #  |   |
-		GPIO.output(D,GPIO.LOW)     #  |___|
-		time.sleep(1)
-		# set address of the tube cathode '1':
-		GPIO.output(A,GPIO.HIGH)    #   /|  
-		GPIO.output(B,GPIO.LOW)     #  / |
-		GPIO.output(C,GPIO.LOW)     #    |
-		GPIO.output(D,GPIO.LOW)     #    |
-		time.sleep(1)
-		# set address of the tube cathode '2':
-		                            #   ___ 
-		GPIO.output(A,GPIO.LOW)     #      |   
-		GPIO.output(B,GPIO.HIGH)    #   ___|   
-		GPIO.output(C,GPIO.LOW)     #  |  
-		GPIO.output(D,GPIO.LOW)     #  |___
-		time.sleep(1)
-		# set address of the tube cathode '3':
-		                            #   ___ 
-		GPIO.output(A,GPIO.HIGH)    #      |   
-		GPIO.output(B,GPIO.HIGH)    #   ___|   
-		GPIO.output(C,GPIO.LOW)     #      |  
-		GPIO.output(D,GPIO.LOW)     #   ___|
-		time.sleep(1)
-		# set address of the tube cathode '4':
-		GPIO.output(A,GPIO.LOW)     #  |   | 
-		GPIO.output(B,GPIO.LOW)     #  |___|   
-		GPIO.output(C,GPIO.HIGH)    #      |  
-		GPIO.output(D,GPIO.LOW)     #      |
-		time.sleep(1)
-		# set address of the tube cathode '5':
-		                            #   ___ 
-		GPIO.output(A,GPIO.HIGH)    #  |      
-		GPIO.output(B,GPIO.LOW)     #  |___   
-		GPIO.output(C,GPIO.HIGH)    #      |  
-		GPIO.output(D,GPIO.LOW)     #   ___|
-		time.sleep(1)
-		# set address of the tube cathode '6':
-		                            #   ___ 
-		GPIO.output(A,GPIO.LOW)     #  |      
-		GPIO.output(B,GPIO.HIGH)    #  |___   
-		GPIO.output(C,GPIO.HIGH)    #  |   |  
-		GPIO.output(D,GPIO.LOW)     #  |___|
-		time.sleep(1)
-		# set address of the tube cathode '7':
-		                            #   ___
-		GPIO.output(A,GPIO.HIGH)    #      |   
-		GPIO.output(B,GPIO.HIGH)    #      |   
-		GPIO.output(C,GPIO.HIGH)    #      |  
-		GPIO.output(D,GPIO.LOW)     #      |
-		time.sleep(1)
-		# set address of the tube cathode '8':
-		                            #   ___ 
-		GPIO.output(A,GPIO.LOW)     #  |   |   
-		GPIO.output(B,GPIO.LOW)     #  |___|   
-		GPIO.output(C,GPIO.LOW)     #  |   |  
-		GPIO.output(D,GPIO.HIGH)    #  |___|
-		time.sleep(1)
-		# set address of the tube cathode '9':
-		                            #   ___ 
-		GPIO.output(A,GPIO.HIGH)    #  |   |   
-		GPIO.output(B,GPIO.LOW)     #  |___|   
-		GPIO.output(C,GPIO.LOW)     #      |  
-		GPIO.output(D,GPIO.HIGH)    #   ___|
-		time.sleep(1)
+                		                  #   ___
+		GPIO.output(bulb[0],GPIO.LOW)     #  |   |
+		GPIO.output(bulb[1],GPIO.LOW)     #  |   |
+		GPIO.output(bulb[2],GPIO.LOW)     #  |   |
+		GPIO.output(bulb[3],GPIO.LOW)     #  |___|
 
-finally:
-	GPIO.cleanup()
+
+def set1(bulb):
+		# set address of the tube cathode '1':
+		GPIO.output(bulb[0],GPIO.HIGH)    #   /|
+		GPIO.output(bulb[1],GPIO.LOW)     #  / |
+		GPIO.output(bulb[2],GPIO.LOW)     #    |
+		GPIO.output(bulb[3],GPIO.LOW)     #    |
+
+
+def set2(bulb):
+		# set address of the tube cathode '2':
+		                                  #   ___
+		GPIO.output(bulb[0],GPIO.LOW)     #      |
+		GPIO.output(bulb[1],GPIO.HIGH)    #   ___|
+		GPIO.output(bulb[2],GPIO.LOW)     #  |
+		GPIO.output(bulb[3],GPIO.LOW)     #  |___
+
+
+def set3(bulb):
+		# set address of the tube cathode '3':
+		                                  #   ___
+		GPIO.output(bulb[0],GPIO.HIGH)    #      |
+		GPIO.output(bulb[1],GPIO.HIGH)    #   ___|
+		GPIO.output(bulb[2],GPIO.LOW)     #      |
+		GPIO.output(bulb[3],GPIO.LOW)     #   ___|
+
+
+def set4(bulb):
+		# set address of the tube cathode '4':
+		GPIO.output(bulb[0],GPIO.LOW)     #  |   |
+		GPIO.output(bulb[1],GPIO.LOW)     #  |___|
+		GPIO.output(bulb[2],GPIO.HIGH)    #      |
+		GPIO.output(bulb[3],GPIO.LOW)     #      |
+
+
+def set5(bulb):
+		# set address of the tube cathode '5':
+		                                  #   ___
+		GPIO.output(bulb[0],GPIO.HIGH)    #  |
+		GPIO.output(bulb[1],GPIO.LOW)     #  |___
+		GPIO.output(bulb[2],GPIO.HIGH)    #      |
+		GPIO.output(bulb[3],GPIO.LOW)     #   ___|
+
+
+def set6(bulb):
+		# set address of the tube cathode '6':
+		                                  #   ___
+		GPIO.output(bulb[0],GPIO.LOW)     #  |
+		GPIO.output(bulb[1],GPIO.HIGH)    #  |___
+		GPIO.output(bulb[2],GPIO.HIGH)    #  |   |
+		GPIO.output(bulb[3],GPIO.LOW)     #  |___|
+
+
+def set7(bulb):
+		# set address of the tube cathode '7':
+		                                  #   ___
+		GPIO.output(bulb[0],GPIO.HIGH)    #      |
+		GPIO.output(bulb[1],GPIO.HIGH)    #      |
+		GPIO.output(bulb[2],GPIO.HIGH)    #      |
+		GPIO.output(bulb[3],GPIO.LOW)     #      |
+
+
+def set8(bulb):
+		# set address of the tube cathode '8':
+		                                  #   ___
+		GPIO.output(bulb[0],GPIO.LOW)     #  |   |
+		GPIO.output(bulb[1],GPIO.LOW)     #  |___|
+		GPIO.output(bulb[2],GPIO.LOW)     #  |   |
+		GPIO.output(bulb[3],GPIO.HIGH)    #  |___|
+
+
+def set9(bulb):
+		# set address of the tube cathode '9':
+		                                  #   ___
+		GPIO.output(bulb[0],GPIO.HIGH)    #  |   |
+		GPIO.output(bulb[1],GPIO.LOW)     #  |___|
+		GPIO.output(bulb[2],GPIO.LOW)     #      |
+		GPIO.output(bulb[3],GPIO.HIGH)    #   ___|
+
+
+def setOff(bulb):
+		# Set GPIO set to inputs so no display is made on the tube
+									 #
+		GPIO.setup(bulb[0], GPIO.IN)
+		GPIO.setup(bulb[1], GPIO.IN)
+		GPIO.setup(bulb[2], GPIO.IN)
+		GPIO.setup(bulb[3], GPIO.IN)
+
+
+def setOn(bulb):
+		# Set GPIO set to outputs so display is enabled and made on the tube
+									 #
+		GPIO.setup(bulb[0], GPIO.OUT)
+		GPIO.setup(bulb[1], GPIO.OUT)
+		GPIO.setup(bulb[2], GPIO.OUT)
+		GPIO.setup(bulb[3], GPIO.OUT)
+
 
