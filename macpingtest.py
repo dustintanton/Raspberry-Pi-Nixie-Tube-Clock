da79d9622fc12a8e7d56a3208c53affc9814b665
Gingie <dustin.tanton@gmail.com>
2020-01-18 18:20:36 -0600

    adding ping program

--- macpingtest.py
+++ macpingtest.py
@@ -1,9 +1,14 @@
 import os
 hostname = "google.com" #example
-response = os.system("ping -c 1 " + hostname)
+mac = ""
 
-#and then check the response...
-if response == 0:
-  print hostname, 'is up!'
-else:
-  print hostname, 'is down!'
+def online(hostname, mac)
+    response = os.system("ping -c 1 " + hostname)
+    #and then check the response...
+    if response == 0:
+        print hostname, 'is up!'
+    else:
+        print hostname, 'is down!'
+
+online(hostname, mac)
+print("done")
