028905b87fbefa76d06701c591c3690259544a77
Gingie <dustin.tanton@gmail.com>
2020-07-09 17:00:58 -0500

    change logic in numbercycle to look matrix-y and much faster fasterer fasterer more randomer added 60 second timeout and auto exit set last numbers so tubes dont light up improperly on exit

--- numbercycle.py
+++ numbercycle.py
@@ -1,9 +1,8 @@
 import time
 import RPi.GPIO as GPIO
 import random
+
 GPIO.setmode(GPIO.BCM)
-
-
 
 # 2nd digit seconds bulb
 #        a  b   c   d
@@ -13,6 +12,7 @@ bulb3 = [6, 13, 19, 26]
 bulb6 = [14, 15, 18, 23]
 bulb5 = [24, 25, 8, 7]
 bulb4 = [12, 16, 20, 21]
+
 
 def main():
     # Time Variables Instances
@@ -24,17 +24,17 @@ def main():
     try:
         while count < 1200:
             # Change random.randint(0, 9) to count if you want to cycle all numbers at the same time
-               findFunctionNumber(random.randint(0, 99) % 10, bulb1)
-               findFunctionNumber(random.randint(0, 99) % 10, bulb2)
-               findFunctionNumber(random.randint(0, 99) % 10, bulb3)
-               findFunctionNumber(random.randint(0, 99) % 10, bulb4)
-               findFunctionNumber(random.randint(0, 99) % 10, bulb5)
-               findFunctionNumber(random.randint(0, 99) % 10, bulb6)
-               print(count)
-               count = count + 1
-               #if (count == 10):
-                   #count = 0
-               time.sleep(.05)
+            findFunctionNumber(random.randint(0, 99) % 10, bulb1)
+            findFunctionNumber(random.randint(0, 99) % 10, bulb2)
+            findFunctionNumber(random.randint(0, 99) % 10, bulb3)
+            findFunctionNumber(random.randint(0, 99) % 10, bulb4)
+            findFunctionNumber(random.randint(0, 99) % 10, bulb5)
+            findFunctionNumber(random.randint(0, 99) % 10, bulb6)
+            print(count)
+            count = count + 1
+            # if (count == 10):
+            # count = 0
+            time.sleep(.05)
     finally:
         setOff(bulb1)
         setOff(bulb2)
@@ -62,6 +62,7 @@ def flicker():
     else:
         time.sleep(.7)
 
+
 def findTube(tube):
     if (tube == 1):
         bulb1
@@ -82,8 +83,9 @@ def findTube(tube):
         bulb6
         return bulb6
 
-def findFunctionNumber(x,bulb):
-    if(x == 0):
+
+def findFunctionNumber(x, bulb):
+    if (x == 0):
         set0(bulb)
 
     if (x == 9):
@@ -127,116 +129,118 @@ def findTime():
     min = localtime.tm_min
     sec = localtime.tm_sec
     print(localtime)
-    print("Hours:",hour)
-    print("Minutes:",min)
-    print("Seconds:",sec)
+    print("Hours:", hour)
+    print("Minutes:", min)
+    print("Seconds:", sec)
 
 
 def set0(bulb):
-        # set address of the tube cathode '0':
-                                          #   ___
-        GPIO.output(bulb[0],GPIO.LOW)     #  |   |
-        GPIO.output(bulb[1],GPIO.LOW)     #  |   |
-        GPIO.output(bulb[2],GPIO.LOW)     #  |   |
-        GPIO.output(bulb[3],GPIO.LOW)     #  |___|
+    # set address of the tube cathode '0':
+    #   ___
+    GPIO.output(bulb[0], GPIO.LOW)  # |   |
+    GPIO.output(bulb[1], GPIO.LOW)  # |   |
+    GPIO.output(bulb[2], GPIO.LOW)  # |   |
+    GPIO.output(bulb[3], GPIO.LOW)  # |___|
 
 
 def set1(bulb):
-        # set address of the tube cathode '1':
-        GPIO.output(bulb[0],GPIO.HIGH)    #   /|
-        GPIO.output(bulb[1],GPIO.LOW)     #  / |
-        GPIO.output(bulb[2],GPIO.LOW)     #    |
-        GPIO.output(bulb[3],GPIO.LOW)     #    |
+    # set address of the tube cathode '1':
+    GPIO.output(bulb[0], GPIO.HIGH)  # /|
+    GPIO.output(bulb[1], GPIO.LOW)  # / |
+    GPIO.output(bulb[2], GPIO.LOW)  # |
+    GPIO.output(bulb[3], GPIO.LOW)  # |
 
 
 def set2(bulb):
-        # set address of the tube cathode '2':
-                                          #   ___
-        GPIO.output(bulb[0],GPIO.LOW)     #      |
-        GPIO.output(bulb[1],GPIO.HIGH)    #   ___|
-        GPIO.output(bulb[2],GPIO.LOW)     #  |
-        GPIO.output(bulb[3],GPIO.LOW)     #  |___
+    # set address of the tube cathode '2':
+    #   ___
+    GPIO.output(bulb[0], GPIO.LOW)  # |
+    GPIO.output(bulb[1], GPIO.HIGH)  # ___|
+    GPIO.output(bulb[2], GPIO.LOW)  # |
+    GPIO.output(bulb[3], GPIO.LOW)  # |___
 
 
 def set3(bulb):
-        # set address of the tube cathode '3':
-                                          #   ___
-        GPIO.output(bulb[0],GPIO.HIGH)    #      |
-        GPIO.output(bulb[1],GPIO.HIGH)    #   ___|
-        GPIO.output(bulb[2],GPIO.LOW)     #      |
-        GPIO.output(bulb[3],GPIO.LOW)     #   ___|
+    # set address of the tube cathode '3':
+    #   ___
+    GPIO.output(bulb[0], GPIO.HIGH)  # |
+    GPIO.output(bulb[1], GPIO.HIGH)  # ___|
+    GPIO.output(bulb[2], GPIO.LOW)  # |
+    GPIO.output(bulb[3], GPIO.LOW)  # ___|
 
 
 def set4(bulb):
-        # set address of the tube cathode '4':
-        GPIO.output(bulb[0],GPIO.LOW)     #  |   |
-        GPIO.output(bulb[1],GPIO.LOW)     #  |___|
-        GPIO.output(bulb[2],GPIO.HIGH)    #      |
-        GPIO.output(bulb[3],GPIO.LOW)     #      |
+    # set address of the tube cathode '4':
+    GPIO.output(bulb[0], GPIO.LOW)  # |   |
+    GPIO.output(bulb[1], GPIO.LOW)  # |___|
+    GPIO.output(bulb[2], GPIO.HIGH)  # |
+    GPIO.output(bulb[3], GPIO.LOW)  # |
 
 
 def set5(bulb):
-        # set address of the tube cathode '5':
-                                          #   ___
-        GPIO.output(bulb[0],GPIO.HIGH)    #  |
-        GPIO.output(bulb[1],GPIO.LOW)     #  |___
-        GPIO.output(bulb[2],GPIO.HIGH)    #      |
-        GPIO.output(bulb[3],GPIO.LOW)     #   ___|
+    # set address of the tube cathode '5':
+    #   ___
+    GPIO.output(bulb[0], GPIO.HIGH)  # |
+    GPIO.output(bulb[1], GPIO.LOW)  # |___
+    GPIO.output(bulb[2], GPIO.HIGH)  # |
+    GPIO.output(bulb[3], GPIO.LOW)  # ___|
 
 
 def set6(bulb):
-        # set address of the tube cathode '6':
-                                          #   ___
-        GPIO.output(bulb[0],GPIO.LOW)     #  |
-        GPIO.output(bulb[1],GPIO.HIGH)    #  |___
-        GPIO.output(bulb[2],GPIO.HIGH)    #  |   |
-        GPIO.output(bulb[3],GPIO.LOW)     #  |___|
+    # set address of the tube cathode '6':
+    #   ___
+    GPIO.output(bulb[0], GPIO.LOW)  # |
+    GPIO.output(bulb[1], GPIO.HIGH)  # |___
+    GPIO.output(bulb[2], GPIO.HIGH)  # |   |
+    GPIO.output(bulb[3], GPIO.LOW)  # |___|
 
 
 def set7(bulb):
-        # set address of the tube cathode '7':
-                                          #   ___
-        GPIO.output(bulb[0],GPIO.HIGH)    #      |
-        GPIO.output(bulb[1],GPIO.HIGH)    #      |
-        GPIO.output(bulb[2],GPIO.HIGH)    #      |
-        GPIO.output(bulb[3],GPIO.LOW)     #      |
+    # set address of the tube cathode '7':
+    #   ___
+    GPIO.output(bulb[0], GPIO.HIGH)  # |
+    GPIO.output(bulb[1], GPIO.HIGH)  # |
+    GPIO.output(bulb[2], GPIO.HIGH)  # |
+    GPIO.output(bulb[3], GPIO.LOW)  # |
 
 
 def set8(bulb):
-        # set address of the tube cathode '8':
-                                          #   ___
-        GPIO.output(bulb[0],GPIO.LOW)     #  |   |
-        GPIO.output(bulb[1],GPIO.LOW)     #  |___|
-        GPIO.output(bulb[2],GPIO.LOW)     #  |   |
-        GPIO.output(bulb[3],GPIO.HIGH)    #  |___|
+    # set address of the tube cathode '8':
+    #   ___
+    GPIO.output(bulb[0], GPIO.LOW)  # |   |
+    GPIO.output(bulb[1], GPIO.LOW)  # |___|
+    GPIO.output(bulb[2], GPIO.LOW)  # |   |
+    GPIO.output(bulb[3], GPIO.HIGH)  # |___|
 
 
 def set9(bulb):
-        # set address of the tube cathode '9':
-                                          #   ___
-        GPIO.output(bulb[0],GPIO.HIGH)    #  |   |
-        GPIO.output(bulb[1],GPIO.LOW)     #  |___|
-        GPIO.output(bulb[2],GPIO.LOW)     #      |
-        GPIO.output(bulb[3],GPIO.HIGH)    #   ___|
+    # set address of the tube cathode '9':
+    #   ___
+    GPIO.output(bulb[0], GPIO.HIGH)  # |   |
+    GPIO.output(bulb[1], GPIO.LOW)  # |___|
+    GPIO.output(bulb[2], GPIO.LOW)  # |
+    GPIO.output(bulb[3], GPIO.HIGH)  # ___|
 
 
 def setOff(bulb):
-        # Set GPIO set to inputs so no display is made on the tube
-                                     #
-        GPIO.setup(bulb[0], GPIO.IN)
-        GPIO.setup(bulb[1], GPIO.IN)
-        GPIO.setup(bulb[2], GPIO.IN)
-        GPIO.setup(bulb[3], GPIO.IN)
-        print("Off")
+    # Set GPIO set to inputs so no display is made on the tube
+    #
+    GPIO.setup(bulb[0], GPIO.IN)
+    GPIO.setup(bulb[1], GPIO.IN)
+    GPIO.setup(bulb[2], GPIO.IN)
+    GPIO.setup(bulb[3], GPIO.IN)
+    print("Off")
+
 
 def setOn(bulb):
-        # Set GPIO set to outputs so display is enabled and made on the tube
-                                     #
-        GPIO.setup(bulb[0], GPIO.OUT)
-        GPIO.setup(bulb[1], GPIO.OUT)
-        GPIO.setup(bulb[2], GPIO.OUT)
-        GPIO.setup(bulb[3], GPIO.OUT)
-        print("On")
+    # Set GPIO set to outputs so display is enabled and made on the tube
+    #
+    GPIO.setup(bulb[0], GPIO.OUT)
+    GPIO.setup(bulb[1], GPIO.OUT)
+    GPIO.setup(bulb[2], GPIO.OUT)
+    GPIO.setup(bulb[3], GPIO.OUT)
+    print("On")
+
 
 setOn(bulb1)
 setOn(bulb2)
@@ -245,5 +249,10 @@ setOn(bulb4)
 setOn(bulb5)
 setOn(bulb6)
 main()
+findFunctionNumber(4, bulb1)
+findFunctionNumber(2, bulb2)
+findFunctionNumber(0, bulb3)
+findFunctionNumber(4, bulb4)
+findFunctionNumber(2, bulb5)
+findFunctionNumber(0, bulb6)
 exit()
-
