3879d2dcce91295a2bc8f8db2b1004c7e1111193
Gingie <dustin.tanton@gmail.com>
2020-07-08 20:34:16 -0500

    tweak time on nixieclock.py

--- nixieclock.py
+++ nixieclock.py
@@ -34,7 +34,7 @@ def main():
 		tenHours = int(hour / 10)
 		minutes = min % 10
 		tenMinutes = int(min / 10)
-		seconds = (sec % 10) + 20
+		seconds = (sec % 10) + 30
 		tenSeconds = int(sec / 10)
 		#findFunctionNumber(seconds, bulb6)
 		while True:
