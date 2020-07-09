66c264055b0a6d181f3d12f99f6cba46cf4e2b8e
Gingie <dustin.tanton@gmail.com>
2020-07-09 16:48:38 -0500

    change logic in numbercycle to look matrix-y and much faster fasterer fasterer

--- numbercycle.py
+++ numbercycle.py
@@ -34,7 +34,7 @@ def main():
                count = count + 1
                if (count == 10):
                    count = 0
-               time.sleep(.1)
+               time.sleep(.05)
     finally:
         setOff(bulb1)
         setOff(bulb2)
