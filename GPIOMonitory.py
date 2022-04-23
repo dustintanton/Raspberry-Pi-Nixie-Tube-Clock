ee00e45f6cc244e8cbda9ef5eb6b69e075a623b2
dustintanton <59024262+dustintanton@users.noreply.github.com>
2022-04-23 15:33:47 -0500

    Create GPIOMonitory.py

---
+++ GPIOMonitory.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+def main():
+
+        while True:
+                x=1
+                while x<27:
+                        GPIO.setup(x, GPIO.IN)
+                        print("Pin ", x, " Value ", GPIO.input(x))
+                        x=x+1
+                time.sleep(1)
+                
+main()
